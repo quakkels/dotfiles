8930e008d31feee2dd088b368c8cb7ec84688867
quakkels <quakkels@gmail.com>
2015-10-18 21:16:58 -0500

    stuff

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-cp vimrc.file $HOME/.vimrc
-cp xinitrc.file $HOME/.xinitrc
-cp Xresources.file $HOME/.Xresources
+cp $HOME/dotfiles/vimrc.file $HOME/.vimrc
+cp $HOME/dotfiles/xinitrc.file $HOME/.xinitrc
+cp $HOME/dotfiles/Xresources.file $HOME/.Xresources
+cp $HOME/dotfiles/bash_profile.file $HOME/.bash_profile
